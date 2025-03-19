7d9790a57457b0a4cac9c2d0198fef44cc3245f4
Svyatoslav Bilalov <133casper@gmail.com>
2025-03-19 19:50:43 +0100

    fix2

--- Gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Bean;
 public class GatewayApplication {
 
     public static void main(String[] args) {
-        System.out.println(System.getenv("TEST_URL"));
         SpringApplication.run(GatewayApplication.class, args);
     }
 
